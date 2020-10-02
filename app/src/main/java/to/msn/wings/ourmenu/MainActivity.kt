9b398ab3f1a86e5738d1db75ec63ef1f45c4512a
kadotaoru <daiyaogata898@gmail.com>
2020-10-02 19:20:22 +0900

    OurMenu

--- app/src/main/java/to/msn/wings/ourmenu/MainActivity.kt
+++ app/src/main/java/to/msn/wings/ourmenu/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        registerForContextMenu(menuImage) //menuImageを長押ししたらonCreateContextMenu()メソッドが呼び出される
+        registerForContextMenu(menuImage) //menuImageを長押ししたらonCreateContextMenu()メソッドが呼び出され
     }
 
     override fun onCreateContextMenu(
